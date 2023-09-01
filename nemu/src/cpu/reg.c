cf35b214f6324f79cd1cf1c32596bd71147f4f74
HHgzs <3166237657@qq.com>
2023-09-02 00:24:07 +0800

    TODO more inv

--- nemu/src/cpu/reg.c
+++ nemu/src/cpu/reg.c
@@ -8,14 +8,16 @@ const char *regsl[] = {"eax", "ecx", "edx", "ebx", "esp", "ebp", "esi", "edi"};
 const char *regsw[] = {"ax", "cx", "dx", "bx", "sp", "bp", "si", "di"};
 const char *regsb[] = {"al", "cl", "dl", "bl", "ah", "ch", "dh", "bh"};
 
-void reg_test() {
+void reg_test()
+{
 	srand(time(0));
 	uint32_t sample[8];
 	uint32_t eip_sample = rand();
 	cpu.eip = eip_sample;
 
 	int i;
-	for(i = R_EAX; i <= R_EDI; i ++) {
+	for (i = R_EAX; i <= R_EDI; i++)
+	{
 		sample[i] = rand();
 		reg_l(i) = sample[i];
 		assert(reg_w(i) == (sample[i] & 0xffff));
@@ -42,30 +44,36 @@ void reg_test() {
 	assert(eip_sample == cpu.eip);
 }
 
-
-
-uint32_t get_reg_val(const char *s, bool *success) {
+uint32_t get_reg_val(const char *s, bool *success)
+{
 	int i;
 	*success = true;
-	for(i = 0; i < 8; i ++) {
-		if(strcmp(regsl[i], s) == 0) {
+	for (i = 0; i < 8; i++)
+	{
+		if (strcmp(regsl[i], s) == 0)
+		{
 			return reg_l(i);
 		}
 	}
 
-	for(i = 0; i < 8; i ++) {
-		if(strcmp(regsw[i], s) == 0) {
+	for (i = 0; i < 8; i++)
+	{
+		if (strcmp(regsw[i], s) == 0)
+		{
 			return reg_w(i);
 		}
 	}
 
-	for(i = 0; i < 8; i ++) {
-		if(strcmp(regsb[i], s) == 0) {
+	for (i = 0; i < 8; i++)
+	{
+		if (strcmp(regsb[i], s) == 0)
+		{
 			return reg_b(i);
 		}
 	}
 
-	if(strcmp("eip", s) == 0) {
+	if (strcmp("eip", s) == 0)
+	{
 		return cpu.eip;
 	}
 
