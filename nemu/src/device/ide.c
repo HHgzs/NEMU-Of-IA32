97a120658ffa3658cc21747ed5a251c03bba0d79
HHgzs <3166237657@qq.com>
2023-09-12 00:33:40 +0800

    PA2 over

--- nemu/src/device/ide.c
+++ nemu/src/device/ide.c
@@ -10,71 +10,83 @@
 #define IDE_IRQ 14
 
 static uint8_t *ide_port_base;
-static uint8_t *bmr_base;	/* bus master registers */
+static uint8_t *bmr_base; /* bus master registers */
 
 static uint32_t sector, disk_idx;
 static uint32_t byte_cnt;
 static bool ide_write;
 static FILE *disk_fp;
 
-void ide_io_handler(ioaddr_t addr, size_t len, bool is_write) {
+void ide_io_handler(ioaddr_t addr, size_t len, bool is_write)
+{
 	assert(byte_cnt <= 512);
 	int ret;
-	if(is_write) {
-		if(addr - IDE_PORT == 0 && len == 4) {
+	if (is_write)
+	{
+		if (addr - IDE_PORT == 0 && len == 4)
+		{
 			/* write 4 bytes data to disk */
 			assert(ide_write);
 			ret = fwrite(ide_port_base, 4, 1, disk_fp);
 			assert(ret == 1);
 
 			byte_cnt += 4;
-			if(byte_cnt == 512) {
+			if (byte_cnt == 512)
+			{
 				/* finish */
 				ide_port_base[7] = 0x40;
 			}
 		}
-		else if(addr - IDE_PORT == 7) {
-			if(ide_port_base[7] == 0x20 || ide_port_base[7] == 0x30) {
+		else if (addr - IDE_PORT == 7)
+		{
+			if (ide_port_base[7] == 0x20 || ide_port_base[7] == 0x30)
+			{
 				/* command: read/write */
-				sector = (ide_port_base[6] & 0x1f) << 24 | ide_port_base[5] << 16
-					| ide_port_base[4] << 8 | ide_port_base[3];
+				sector = (ide_port_base[6] & 0x1f) << 24 | ide_port_base[5] << 16 | ide_port_base[4] << 8 | ide_port_base[3];
 				disk_idx = sector << 9;
 				fseek(disk_fp, disk_idx, SEEK_SET);
 
 				byte_cnt = 0;
 
-				if(ide_port_base[7] == 0x20) {
+				if (ide_port_base[7] == 0x20)
+				{
 					/* command: read from disk */
 					ide_write = false;
 					ide_port_base[7] = 0x40;
 					i8259_raise_intr(IDE_IRQ);
 				}
-				else {
+				else
+				{
 					/* command: write to disk */
 					ide_write = true;
 				}
 			}
-			else if (ide_port_base[7] == 0xc8) {
+			else if (ide_port_base[7] == 0xc8)
+			{
 				/* command: DMA read */
 
 				/* Nothing to do here. The actual read operation is
 				 * issued by write commands to the bus master register. */
 			}
-			else {
+			else
+			{
 				/* not implemented command */
 				assert(0);
 			}
 		}
 	}
-	else {
-		if(addr - IDE_PORT == 0 && len == 4) {
+	else
+	{
+		if (addr - IDE_PORT == 0 && len == 4)
+		{
 			/* read 4 bytes data from disk */
 			assert(!ide_write);
 			ret = fread(ide_port_base, 4, 1, disk_fp);
 			assert(ret == 1 || feof(disk_fp));
 
 			byte_cnt += 4;
-			if(byte_cnt == 512) {
+			if (byte_cnt == 512)
+			{
 				/* finish */
 				ide_port_base[7] = 0x40;
 			}
@@ -82,13 +94,18 @@ void ide_io_handler(ioaddr_t addr, size_t len, bool is_write) {
 	}
 }
 
-void bmr_io_handler(ioaddr_t addr, size_t len, bool is_write) {
+void bmr_io_handler(ioaddr_t addr, size_t len, bool is_write)
+{
 	int ret;
-	if(is_write) {
-		if(addr - BMR_PORT == 0) {
-			if(bmr_base[0] & 0x1) {
+	if (is_write)
+	{
+		if (addr - BMR_PORT == 0)
+		{
+			if (bmr_base[0] & 0x1)
+			{
 				/* DMA start command */
-				if(bmr_base[0] & 0x8) {
+				if (bmr_base[0] & 0x8)
+				{
 					/* DMA read */
 
 					/* the address of Physical Region Descriptor Table */
@@ -98,8 +115,7 @@ void bmr_io_handler(ioaddr_t addr, size_t len, bool is_write) {
 					uint32_t hi_entry = hwaddr_read(prdt_addr + 4, 4);
 					uint16_t byte_cnt = hi_entry & 0xffff;
 
-					sector = (ide_port_base[6] & 0x1f) << 24 | ide_port_base[5] << 16
-						| ide_port_base[4] << 8 | ide_port_base[3];
+					sector = (ide_port_base[6] & 0x1f) << 24 | ide_port_base[5] << 16 | ide_port_base[4] << 8 | ide_port_base[3];
 					disk_idx = sector << 9;
 					fseek(disk_fp, disk_idx, SEEK_SET);
 
@@ -113,7 +129,8 @@ void bmr_io_handler(ioaddr_t addr, size_t len, bool is_write) {
 					ide_port_base[7] = 0x40;
 					i8259_raise_intr(IDE_IRQ);
 				}
-				else {
+				else
+				{
 					/* DMA write is not implemented */
 					assert(0);
 				}
@@ -122,7 +139,8 @@ void bmr_io_handler(ioaddr_t addr, size_t len, bool is_write) {
 	}
 }
 
-void init_ide() {
+void init_ide()
+{
 	ide_port_base = add_pio_map(IDE_PORT, 8, ide_io_handler);
 	ide_port_base[7] = 0x40;
 
